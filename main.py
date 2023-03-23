86a2eb95083141519810a0feed438d1710608c94
Daniel <ddkozhaev@gmail.com>
2023-03-24 02:08:25 +0300

    Даня

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # pip install -r requirements.txt
+from utils import *
 
 
 if __name__ == "__main__":
-    print('its main')
-    print('Привет')
+    with open('text.txt', 'r', encoding='utf-8') as f:
+        text = f.read()
+    text = re.sub(r'[^А-Яа-яёЁ1-9,*0.]', '', text)
+    text = list(text)
+    list_upper_case = [i for i in text if i.isupper()]
+    value_passing_fingers(0, len(list_upper_case) + 2)
+    text = [i.lower() for i in text]
+    print(text)
+    for i in range(1, len(text)):
+        count_steps(text[i - 1], text[i])
+    print(counter_fingers)
--- utils.py
+++ utils.py
@@ -1,5 +1,44 @@
-def funcname(a, b):
-    return a / b
+import json
+import re
+import numpy
 
-print(funcname(10, 5))
-print('hui')
+
+counter_fingers = {'f5l': 0, 'f4l': 0, 'f3l': 0, 'f2l': 0, 'f1l': 0, 'f1r': 0, 'f2r': 0, 'f3r': 0, 'f4r': 0, 'f5r': 0}
+
+
+def create_dict():
+    with open('diktor.json', encoding='utf-8') as kb_json:
+        data_dict = json.load(kb_json)
+    return data_dict
+
+
+def get_cords(symbol):
+    data_dict = create_dict()
+    for key in data_dict:
+        for value in data_dict[key]['key']:
+            if value == symbol:
+                return [data_dict[key]['raw'], data_dict[key]['column']]
+
+
+def value_passing_fingers(column, value):
+    match column:
+        case 0 | 1:
+            counter_fingers['f5l'] += value
+        case 2:
+            counter_fingers['f4l'] += value
+        case 3:
+            counter_fingers['f3l'] += value
+        case 4 | 5:
+            counter_fingers['f2l'] += value
+        case 6 | 7:
+            counter_fingers['f2r'] += value
+        case 8:
+            counter_fingers['f3r'] += value
+        case 9:
+            counter_fingers['f4r'] += value
+        case 10 | 11 | 12:
+            counter_fingers['f5r'] += value
+
+
+def count_steps(first_sim, second_sim):
+    return 228
